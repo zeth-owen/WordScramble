177f777f42b55e6eb056a4c2914cc55e936156f4
Zeth Thomas <zeththomas89@gmail.com>
2024-11-15 12:32:42 -0800

    Initial Commit

---
+++ WordScrambleTests/WordScrambleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleTests.swift
+//  WordScrambleTests
+//
+//  Created by Zeth Thomas on 11/15/24.
+//
+
+import Testing
+@testable import WordScramble
+
+struct WordScrambleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
